ef0e0378290e5f30f7a46bd76ab03de0fd221703
jersondereal <jersoncaibog1@gmail.com>
2025-05-06 09:34:34 +0800

    database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql');
 require('dotenv').config();
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: "sql211.infinityfree.com",
-    user: "if0_38903994",
-    password: "Rb2gnefCfN",
-    database: "if0_38903994_dareen_gutsco",
-    port: "3306",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
